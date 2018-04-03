69b005021fa1eabeabb3a1684f154703a44abdce
Leo Arias <yo@elopio.net>
2018-04-03 14:41:30 +0000

    Fix spaces

--- test/C3Linearization.test.js
+++ test/C3Linearization.test.js
@@ -11,24 +11,24 @@ require('chai')
 contract('C3Linearization', function () {
     it('should linearize the inheritance graph', async function () {
         let contractA = await A.new();
-	let contractB = await B.new();
-	let contractC = await C.new();
-	let contractD = await D.new();
+        let contractB = await B.new();
+        let contractC = await C.new();
+        let contractD = await D.new();
 
-	let logs = null;
-	({ logs } = await contractD.f());
-	logs.length.should.be.equal(4);
-	logs[0].event.should.be.eq('somethingD');
-	logs[1].event.should.be.eq('somethingC');
-	logs[2].event.should.be.eq('somethingB');
+        let logs = null;
+        ({ logs } = await contractD.f());
+        logs.length.should.be.equal(4);
+        logs[0].event.should.be.eq('somethingD');
+        logs[1].event.should.be.eq('somethingC');
+        logs[2].event.should.be.eq('somethingB');
         logs[3].event.should.be.eq('somethingA');
 
-	let contractInvertedD = await invertedD.new();
-	({ logs } = await contractInvertedD.f());
-	logs.length.should.be.equal(4);
-	logs[0].event.should.be.eq('somethingD');
-	logs[1].event.should.be.eq('somethingB');
-	logs[2].event.should.be.eq('somethingC');
+        let contractInvertedD = await invertedD.new();
+        ({ logs } = await contractInvertedD.f());
+        logs.length.should.be.equal(4);
+        logs[0].event.should.be.eq('somethingD');
+        logs[1].event.should.be.eq('somethingB');
+        logs[2].event.should.be.eq('somethingC');
         logs[3].event.should.be.eq('somethingA');
     });
 });
